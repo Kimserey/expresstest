5bf726c34f08f50943f16f1078215880a36afd63
Kimserey <kimserey.lam@gmail.com>
2014-08-03 01:31:43 +0800

    added index.html and middleware for static file in app.js

--- app.js
+++ app.js
@@ -15,12 +15,14 @@
 var http    = require('http'),
 	express = require('express'),
 	bodyParser = require('body-parser'),
-	connectionString = process.argv[2] || 'mongodb://localhost/expresstestDb_test',
+	port = Number(process.argv[2]) || 3000,
+	connectionString = process.argv[3] || 'mongodb://localhost/expresstestDb_test',
 	router  = require('./routes')(connectionString),
 	app     = express(),
 	server  = http.createServer(app),
 	env = process.env.NODE_ENV || 'development';
 
+// ----- Begin Middleware -----
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
@@ -29,25 +31,30 @@ app.use(bodyParser.json());
 
 if (env === 'development') {
 	app.use(function (req, res, next) {
-		var date = new Date(),
-			dateString = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-
-		console.log('[' + dateString + '][' + req.method + '] : ' + req.path
-			+ ' --- req body : ' + JSON.stringify(req.body));
+		var date = new Date().getHours() + ':' + new Date().getMinutes() + ':' + new Date().getSeconds();
+		console.log('[' + date + '][' + req.method + '] : ' 
+			+ req.path + ' --- req body : ' + JSON.stringify(req.body));
 		next();
 	});
 }
 
+app.use(express.static(__dirname + '/public'));
 app.use('/api', router);
+// ----- End Middleware -----
 
-server.listen(3000);
+// ----- Begin routes -----
+app.get('/', function (req, res) {
+	res.redirect('/index.html');
+});
+// ----- End routes -----
 
+server.listen(port);
 console.log(
 	'\n\n'
 	+ '------------------\n' 
 	+ '- Server started -\n'
 	+ '------------------\n' 
-	+ 'port           : ' + 3000 + ',\n' 
+	+ 'port           : ' + port + ',\n' 
 	+ 'env            : '  + env  + ',\n'
 	+ 'MongoDB string : ' + connectionString 
 	+ '\n\n');
